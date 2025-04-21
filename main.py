6b4a5bf1401505f5e5a069914c2c9f568718d111
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-04-21 06:06:27 -0400

    handle exception at top level of main.py

--- main.py
+++ main.py
@@ -9,13 +9,9 @@ try:
     zd.start (arg="", Part_name="main", palette=palette, env=env)
 except Exception as e:
     _, _, tb = sys.exc_info()
-    print (f'\n\n\n*** at line {tb.tb_lineno}: {e}', file=sys.stderr)
-    #print(f"{type(e).__name__} at line {tb.tb_lineno}: {e}")
     while tb.tb_next:
         tb = tb.tb_next
-    print(f"{type(e).__name__} at line {tb.tb_lineno}: {e}", file=sys.stderr)    
-    #print(f'zzz at line {tb.tb_lineno}: {e}', file=sys.stderr)    
     frame = tb.tb_frame
     filename = frame.f_code.co_filename
     line_number = tb.tb_lineno
-    print(f"{type(e).__name__} at {filename}:{line_number}: {e}", file=sys.stderr)
+    print(f"\n\n\n*** {type(e).__name__} at {filename}:{line_number}: {e}", file=sys.stderr)
