b0eaecfae6a2ff15c1518f839f2426d0d4a47f5a
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-04-21 05:50:40 -0400

    exception handling at top level

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ sys.path.insert(0, './zd')
 import kernel0d as zd
 
 #import display
-
-[palette, env] = zd.initialize_from_files (".", ["part.drawio.json"])
-#display.Display_install (palette)
-zd.start (arg="", Part_name="main", palette=palette, env=env)
+try:
+    [palette, env] = zd.initialize_from_files (".", ["part.drawio.json"])
+    #display.Display_install (palette)
+    zd.start (arg="", Part_name="main", palette=palette, env=env)
+except Exception as e:
+    print (f'\n\n\n*** {e}', file=sys.stderr)
+    
