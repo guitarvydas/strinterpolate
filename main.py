4ae22cff0a4e660fc586599fa0d0441a13c35acc
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-04-21 06:05:10 -0400

    save (playing with exceptions)

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ try:
     #display.Display_install (palette)
     zd.start (arg="", Part_name="main", palette=palette, env=env)
 except Exception as e:
-    print (f'\n\n\n*** {e}', file=sys.stderr)
-    
+    _, _, tb = sys.exc_info()
+    print (f'\n\n\n*** at line {tb.tb_lineno}: {e}', file=sys.stderr)
+    #print(f"{type(e).__name__} at line {tb.tb_lineno}: {e}")
+    while tb.tb_next:
+        tb = tb.tb_next
+    print(f"{type(e).__name__} at line {tb.tb_lineno}: {e}", file=sys.stderr)    
+    #print(f'zzz at line {tb.tb_lineno}: {e}', file=sys.stderr)    
+    frame = tb.tb_frame
+    filename = frame.f_code.co_filename
+    line_number = tb.tb_lineno
+    print(f"{type(e).__name__} at {filename}:{line_number}: {e}", file=sys.stderr)
